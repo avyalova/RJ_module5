f3dd713a721375b53aa377958d8a2c8d69f6b36b
Anna <anna_vyalova@yahoo.com>
2023-01-21 13:38:09 +0100

    Modify task based on comments from Alexandr

--- task_scrore.js
+++ task_scrore.js
@@ -3,6 +3,6 @@ let a = 5 //балы за введение
 let b = 6 //баллы за модуль git
 let c = 7 //баллы за модуль js
 let d = 3 //количество модулей
-let e = (a + b + c) / d //средний балл
+//let e = (a + b + c) / d //средний балл
 
-console.log("Средний бал", fullName, e)
+console.log("Итоговый балл:" + "" + fullName + " - " + (a + b + c) / d)
