a6f65d1f0c15a885c8d2f174e7d0d0f463fac04d
Anna <anna_vyalova@yahoo.com>
2023-01-25 20:17:09 +0100

    Modify changes

---
+++ Task-7-2V2-1.js
@@ -0,0 +1,30 @@
+const players = [
+  {
+    id: 1,
+    name: "Ivan",
+    scorePoints: 4500,
+  },
+  {
+    id: 2,
+    name: "Petr",
+    scorePoints: 3600,
+  },
+  {
+    id: 3,
+    name: "Vadim",
+    scorePoints: 3433,
+  },
+  {
+    id: 4,
+    name: "Olga",
+    scorePoints: 2356,
+  },
+]
+
+let scorePlayerPoint = []
+
+for (let player of players) {
+  scorePlayerPoint.push(player.scorePoints)
+}
+
+console.log(Math.max(...scorePlayerPoint))
