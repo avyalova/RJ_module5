b6bf2556d7be474b247407b155950adc135087a5
Anna <anna_vyalova@yahoo.com>
2023-01-25 19:30:10 +0100

    Taks_7-2

---
+++ Task_7-2.js
@@ -0,0 +1,28 @@
+const players = [
+  {
+    id: 1,
+    name: "Ivan",
+    scorePoints: 4500,
+  },
+  {
+    id: 2,
+    name: "Petr",
+    scorePoints: 3600,
+  },
+  {
+    id: 3,
+    name: "Vadim",
+    scorePoints: 3433,
+  },
+  {
+    id: 4,
+    name: "Olga",
+    scorePoints: 2356,
+  },
+]
+
+let resultOne = players.find(function (item) {
+  return Math.max(item.scorePoints)
+})
+
+console.log(resultOne)
